5cf5a4e30c20a5c78f72e1038f2c695ca7484133
thona6473 <thona6473@gmail.com>
2024-12-08 23:59:49 +0700

    add admin role

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,22 +1,22 @@
-import express from "express";
-import verifyToken from "../middleware/authMiddleware";
-import adminController from "../controller/AdminController";
-
-const router = express.Router();
-
-// Admin Dashboard
-router.get("/dashboard", verifyToken("admin"), (req, res) => {
-    res.json({ message: "Admin authenticated", admin: req.user });
-});
-
-// Admin-specific actions (examples)
-router.get("/users", verifyToken("admin"), adminController.getAllUsers); // Fetch all users
-router.post("/create-user", verifyToken("admin"), adminController.createUser); // Create a user
-router.delete("/delete-user/:id", verifyToken("admin"), adminController.deleteUser); // Delete a user by ID
-router.put("/update-user/:id", verifyToken("admin"), adminController.updateUser); // Update user details
-
-// Admin Profile
-router.get("/profile", verifyToken("admin"), adminController.getProfile); // Get admin profile
-router.put("/profile", verifyToken("admin"), adminController.updateProfile); // Update admin profile
-
-export default router;
+// import express from "express";
+// import verifyToken from "../middleware/authMiddleware";
+// import adminController from "../controller/AdminController";
+//
+// const router = express.Router();
+//
+// // Admin Dashboard
+// router.get("/dashboard", verifyToken("admin"), (req, res) => {
+//     res.json({ message: "Admin authenticated", admin: req.user });
+// });
+//
+// // Admin-specific actions (examples)
+// router.get("/users", verifyToken("admin"), adminController.getAllUsers); // Fetch all users
+// router.post("/create-user", verifyToken("admin"), adminController.createUser); // Create a user
+// router.delete("/delete-user/:id", verifyToken("admin"), adminController.deleteUser); // Delete a user by ID
+// router.put("/update-user/:id", verifyToken("admin"), adminController.updateUser); // Update user details
+//
+// // Admin Profile
+// router.get("/profile", verifyToken("admin"), adminController.getProfile); // Get admin profile
+// router.put("/profile", verifyToken("admin"), adminController.updateProfile); // Update admin profile
+//
+// export default router;
