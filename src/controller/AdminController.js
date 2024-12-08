ecf05a66750997672ab29f0d1dda19c2e563a0e0
thona6473 <thona6473@gmail.com>
2024-12-09 00:01:43 +0700

    add admin role

--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -1,26 +1,25 @@
-
-const adminController = {
-    getAllUsers: async (req, res) => {
-
-    },
-
-    createUser: async (req, res) => {
-    },
-
-    deleteUser: async (req, res) => {
-    },
-
-    updateUser: async (req, res) => {
-
-    },
-
-    getProfile: async (req, res) => {
-
-    },
-
-    updateProfile: async (req, res) => {
-
-    },
-};
-
-export default adminController;
+//
+// const adminController = {
+//     getAllUsers: async (req, res) => {
+//     },
+//
+//     createUser: async (req, res) => {
+//     },
+//
+//     deleteUser: async (req, res) => {
+//     },
+//
+//     updateUser: async (req, res) => {
+//
+//     },
+//
+//     getProfile: async (req, res) => {
+//
+//     },
+//
+//     updateProfile: async (req, res) => {
+//
+//     },
+// };
+//
+// export default adminController;
