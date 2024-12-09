0cc034b8082d5e06b80a7cf77171f5448fef7818
thona6473 <thona6473@gmail.com>
2024-12-09 16:14:52 +0700

    V1 added admin complete

--- src/middleware/AdminAuth.js
+++ src/middleware/AdminAuth.js
@@ -7,21 +7,16 @@ function AdminAuth(req, res, next) {
     if (!token) {
         return res.status(401).json({ message: "Authentication required" });
     }
-
     jwt.verify(token, process.env.JWT_KEY, (err, admin) => {
         if (err) {
             return res.status(401).json({ message: "Invalid or expired token" });
         }
 
-        // Log the decoded token for debugging
-        console.log('Decoded token:', admin);
-
-        // Ensure the payload has the expected structure
         if (!admin || !admin.data) {
             return res.status(401).json({ message: "Invalid token structure" });
         }
 
-        req.admin = admin.data; // Set the admin data in the request object
+        req.admin = admin.data;
         next();
     });
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,28 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const authController = require("../controller/AuthController");
-// const multer = require("multer");
-// const verifyToken = require('../middleware/authMiddleware');
-//
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'upload');
-//   },
-//   filename: function (req, file, cb) {
-//     const filename = file.fieldname + '_' + Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpg';
-//     req.body.profile_picture_path = filename;
-//     cb(null, filename);
-//   }
-// });
-//
-// const upload = multer({ storage: storage });
-//
-// router.post("/register", upload.single('image'), authController.register);
-// router.post("/login", authController.login);
-// router.get("/check-auth", verifyToken, authController.checkAuth);
-//
-//
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();
@@ -31,8 +6,6 @@ const multer = require("multer");
 const verifyToken = require('../middleware/authMiddleware');
 const AdminAuth = require('../middleware/AdminAuth');
 
-
-// Multer storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'upload');
@@ -46,12 +19,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// User routes
 router.post("/register", upload.single('image'), authController.register);
 router.post("/login", authController.login);
 router.get("/check-auth", verifyToken, authController.checkAuth);
 
-// Admin routes
 router.post("/admin/register", upload.single('image'), authController.adminRegister);
 router.post("/admin/login", authController.adminLogin);
 router.get("/admin/check-auth", AdminAuth,authController.checkAdminAuth);
