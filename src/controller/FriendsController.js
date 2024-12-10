8d09206eb5a5c371de69b7fa98dbf61e594c953a
thona6473 <thona6473@gmail.com>
2024-12-10 15:55:39 +0700

    resolve get friend

--- src/controller/FriendsController.js
+++ src/controller/FriendsController.js
@@ -64,7 +64,8 @@
 //         }
 //     }
 // }
-// module.exports = FriendController;
+module.exports = FriendController;
+
 const User = require('../model/User');
 
 const FriendController = {
@@ -101,25 +102,69 @@ const FriendController = {
         }
     },
 
+    // getAllFriendAndNotFriend: async (req, res) => {
+    //     try {
+    //         const { id } = req.params;
+    //         const user = await User.findById(id);
+    //         if (!user) {
+    //             return res.status(404).json({ message: "User not found." });
+    //         }
+    //
+    //         const friends = await User.find({ _id: { $in: user.friends } }).select('id username profile_picture_path');
+    //         const notFriends = await User.find({ _id: { $nin: [...user.friends, id] } }).select('id username profile_picture_path');
+    //
+    //         res.status(200).json({
+    //             friends,
+    //             not_friends: notFriends,
+    //         });
+    //     } catch (error) {
+    //         res.status(500).json({ message: error.message });
+    //     }
+    // },
+
     getAllFriendAndNotFriend: async (req, res) => {
         try {
-            const { id } = req.params;
+            const {id} = req.params;
             const user = await User.findById(id);
-            if (!user) {
-                return res.status(404).json({ message: "User not found." });
-            }
-
-            const friends = await User.find({ _id: { $in: user.friends } }).select('id username profile_picture_path');
-            const notFriends = await User.find({ _id: { $nin: [...user.friends, id] } }).select('id username profile_picture_path');
-
+            const friends = [];
+            await Promise.all(
+                user.friends.map(async (friend_id) => {
+                    const friend = await User.findById(friend_id);
+                    if (friend) {
+                        const formated = {
+                            _id: friend.id,
+                            username: friend.username,
+                            profile_picture_path: friend.profile_picture_path,
+                        }
+                        friends.push(formated);
+                    }
+                })
+            )
+            const all_user = await User.find();
+            const not_friends = [];
+            await Promise.all(
+                all_user.map(async (single_user) => {
+                    if (!user.friends.includes(single_user.id)) {
+                        const not_friend = await User.findById(single_user.id);
+                        if (not_friend && single_user.id !== id) {
+                            const formated = {
+                                _id: not_friend.id,
+                                username: not_friend.username,
+                                profile_picture_path: not_friend.profile_picture_path,
+                            }
+                            not_friends.push(formated);
+                        }
+                    }
+                })
+            );
             res.status(200).json({
                 friends,
-                not_friends: notFriends,
+                not_friends,
             });
         } catch (error) {
-            res.status(500).json({ message: error.message });
+            res.status(400).json({message: error.message});
         }
-    },
+    }
 };
 
 module.exports = FriendController;
